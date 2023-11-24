565e7298bcc408e03ef686bfc0f0bc74bf076fb1
Suyash Bavalekar <50289222+Suyash777@users.noreply.github.com>
2023-11-24 10:03:35 +0530

    Add files via upload

---
+++ order-services/src/main/java/com/demo/controllers/CartController.java
@@ -0,0 +1,52 @@
+package com.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.demo.dto.CartDTO;
+import com.demo.services.CartService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/carts")
+public class CartController {
+
+    private final CartService cartService;
+
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<CartDTO> createCart(@PathVariable Long userId, @RequestBody CartDTO cartDTO) {
+        return cartService.createCart(userId, cartDTO);
+    }
+
+    @GetMapping("/{cartId}")
+    public ResponseEntity<CartDTO> getCartById(@PathVariable Long cartId) {
+        return cartService.getCartById(cartId);
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<CartDTO>> getCartsByUserId(@PathVariable Long userId) {
+        return cartService.getCartsByUserId(userId);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartDTO>> getAllCarts() {
+        return cartService.getAllCarts();
+    }
+
+    @PutMapping("/{cartId}")
+    public ResponseEntity<Void> updateCart(@PathVariable Long cartId, @RequestBody CartDTO cartDTO) {
+        return cartService.updateCart(cartId, cartDTO);
+    }
+
+    @DeleteMapping("/{cartId}")
+    public ResponseEntity<Void> deleteCart(@PathVariable Long cartId) {
+        return cartService.deleteCart(cartId);
+    }
+}
---
+++ order-services/src/main/java/com/demo/services/CartService.java
@@ -0,0 +1,23 @@
+package com.demo.services;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+import com.demo.dto.CartDTO;
+import com.demo.dto.LineItemDTO;
+
+public interface CartService {
+	ResponseEntity<CartDTO> createCart(Long customerId, CartDTO cartDTO);
+
+    ResponseEntity<CartDTO> getCartById(Long cartId);
+
+    ResponseEntity<List<CartDTO>> getCartsByUserId(Long customerId);
+
+    ResponseEntity<List<CartDTO>> getAllCarts();
+
+    ResponseEntity<Void> updateCart(Long cartId, CartDTO cartDTO);
+
+    ResponseEntity<Void> deleteCart(Long cartId);
+}
+
